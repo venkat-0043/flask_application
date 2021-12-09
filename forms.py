0d2955642a156de6b2af58e81feaf97dc691c7a6
venkat-0043 <venkatareddy.vanipanti@modernchipsolutions.com>
2021-12-09 11:13:37 +0530

    first commit

---
+++ forms.py
@@ -0,0 +1,2 @@
+import flask_wtf
+print(dir(flask_wtf))
---
+++ sample.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def func():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
